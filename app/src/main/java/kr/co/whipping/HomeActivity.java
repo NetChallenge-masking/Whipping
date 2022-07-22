20febb1b0343830c849257af5549995d07245427
cloudwoon <cloud.wooo0n@gmail.com>
2022-07-22 17:55:49 +0900

    [FEAT] 기본 틀 생성

---
+++ app/src/main/java/kr/co/whipping/HomeActivity.java
@@ -0,0 +1,41 @@
+package kr.co.whipping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void finishShopping(View view) {
+        finish();
+    }
+
+    public void goCart(View view) {
+        Intent intent = new Intent(getApplicationContext(), CartActivity.class);
+        startActivity(intent);
+    }
+
+    public void goScan(View view) {
+        Intent intent = new Intent(getApplicationContext(), ScanActivity.class);
+        startActivity(intent);
+    }
+
+    public void goLocation(View view) {
+        Intent intent = new Intent(getApplicationContext(), LocationActivity.class);
+        startActivity(intent);
+    }
+
+    public void goSearch(View view) {
+        Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+        startActivity(intent);
+    }
+
+}
--- app/src/main/java/kr/co/whipping/MainActivity.java
+++ app/src/main/java/kr/co/whipping/MainActivity.java
@@ -2,7 +2,9 @@ package kr.co.whipping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void startShopping(View view) {
+        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(intent);
     }
 }
