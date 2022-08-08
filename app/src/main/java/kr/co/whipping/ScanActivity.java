9e3447bf66a8209e55b2a0694bbfe8763d4afaea
@1ss1222 <1ss1222@naver.com>
2022-08-08 23:16:36 +0900

    바코드 스캔, 생성 코드 추가

--- app/src/main/java/kr/co/whipping/ScanActivity.java
+++ app/src/main/java/kr/co/whipping/ScanActivity.java
@@ -2,7 +2,12 @@ package kr.co.whipping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import kr.co.whipping.scan.barcordscan.BarcodeScanActivity;
 
 public class ScanActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class ScanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        Button barcodeRecog = (Button) findViewById(R.id.barcordscanBtn);
+        barcodeRecog.setOnClickListener(new View.OnClickListener(){
+            @Override public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), BarcodeScanActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/kr/co/whipping/scan/barcordscan/BarcodeScanActivity.java
+++ app/src/main/java/kr/co/whipping/scan/barcordscan/BarcodeScanActivity.java
@@ -1,14 +1,64 @@
 package kr.co.whipping.scan.barcordscan;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import kr.co.whipping.R;
+
 public class BarcodeScanActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode);
     }
+    public void onClick(View V){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            String barcodenum;
+            String barcodetype;
+
+            barcodenum = scanResult.getContents();
+            barcodetype = scanResult.getFormatName();
+
+            TextView etBarcode = (TextView) findViewById(R.id.etBarcode);
+            TextView etTyp = (TextView) findViewById(R.id.etTyp);
+            etBarcode.setText(barcodenum);
+            etTyp.setText(barcodetype);
+
+            ImageView img_barcode;
+            img_barcode = (ImageView)findViewById(R.id.img_barcode) ;
+
+            MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+            final int WIDTH = 360;
+            final int HEIGHT = 180;
+
+            try {
+                BitMatrix bitMatrix = multiFormatWriter.encode(barcodenum, BarcodeFormat.valueOf(barcodetype), WIDTH, HEIGHT);
+                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                img_barcode.setImageBitmap(bitmap);
+            } catch (Exception e) {
+            }
+        }
+    }
+
 }
