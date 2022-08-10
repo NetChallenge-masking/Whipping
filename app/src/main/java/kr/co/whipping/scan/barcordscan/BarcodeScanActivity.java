dbedda45e44fb7a89beea69a5c3937a823db3d12
@1ss1222 <1ss1222@naver.com>
2022-08-10 15:36:41 +0900

    바코드 스캔 코드 수정 및 xml 작성

--- app/src/main/java/kr/co/whipping/scan/barcordscan/BarcodeScanActivity.java
+++ app/src/main/java/kr/co/whipping/scan/barcordscan/BarcodeScanActivity.java
@@ -4,10 +4,10 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.BarcodeFormat;
@@ -23,11 +23,16 @@ public class BarcodeScanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode);
+
+        new IntentIntegrator(this).initiateScan();
+
+//        IntentIntegrator integrator = new IntentIntegrator(this);
+//        integrator.initiateScan();
     }
-    public void onClick(View V){
-        IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.initiateScan();
-    }
+//    public void onClick(View V) {
+//        IntentIntegrator integrator = new IntentIntegrator(this);
+//        integrator.initiateScan();
+//    }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
@@ -39,17 +44,28 @@ public class BarcodeScanActivity extends AppCompatActivity {
             barcodenum = scanResult.getContents();
             barcodetype = scanResult.getFormatName();
 
-            TextView etBarcode = (TextView) findViewById(R.id.etBarcode);
-            TextView etTyp = (TextView) findViewById(R.id.etTyp);
-            etBarcode.setText(barcodenum);
-            etTyp.setText(barcodetype);
+//            Button minus = (Button) findViewById(R.id.minus);
+//            Button plus = (Button) findViewById(R.id.plus);
+//            Button cancel = (Button) findViewById(R.id.cancel);
+//            Button add = (Button) findViewById(R.id.add);
+//
+//            TextView category = (TextView) findViewById(R.id.category);
+//            TextView nameOfprod = (TextView) findViewById(R.id.nameOfProd);
+//            TextView price = (TextView) findViewById(R.id.price);
+
+            //DB에서 상품 정보 가져오는 코드 추가
+
+//            TextView etBarcode = (TextView) findViewById(R.id.etBarcode);
+//            TextView etTyp = (TextView) findViewById(R.id.etTyp);
+//            etBarcode.setText(barcodenum);
+//            etTyp.setText(barcodetype);
 
             ImageView img_barcode;
             img_barcode = (ImageView)findViewById(R.id.img_barcode) ;
 
             MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
-            final int WIDTH = 360;
-            final int HEIGHT = 180;
+            final int WIDTH = 180;
+            final int HEIGHT = 90;
 
             try {
                 BitMatrix bitMatrix = multiFormatWriter.encode(barcodenum, BarcodeFormat.valueOf(barcodetype), WIDTH, HEIGHT);
