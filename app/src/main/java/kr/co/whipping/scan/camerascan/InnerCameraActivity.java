eea4d12d7c9d0e0db5987f14f2a2360062f40aba
aerim-choi <sswuaerim@naver.com>
2022-09-18 04:36:27 +0900

    [REFACTOR] 내장카메라와 텍스트 인식 코드 리펙토링
    - 내장 카메라기능과 텍스트인식 코드 연결
    - InnerCameraActivity코드에 CameraScanActivity코드의 텍스트인식 코드 부분을 추가하여 구현
    - CameraScanActivity에 InnerCameraActivity의 결과값을 넘겨받아 텍스트인식 결과를 출력하도록 수정 필요

--- app/src/main/java/kr/co/whipping/scan/camerascan/InnerCameraActivity.java
+++ app/src/main/java/kr/co/whipping/scan/camerascan/InnerCameraActivity.java
@@ -22,7 +22,9 @@ import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -33,40 +35,59 @@ import android.util.Size;
 import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 
+import kr.co.whipping.MainActivity;
 import kr.co.whipping.R;
 
 public class InnerCameraActivity extends AppCompatActivity implements ImageAnalysis.Analyzer, View.OnClickListener {
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
-
+    private static final String CLOUD_VISION_API_KEY = "AIzaSyDaNsgeV__6hcIrCllAdU1XAgj4OV29id4";
+    private static final String TAG = MainActivity.class.getSimpleName();
     PreviewView previewView;
     private ImageCapture imageCapture;
-    private VideoCapture videoCapture;
-    private Button bRecord;
     private Button bCapture;
+    private TextView mImageDetails;
+    private static final int MAX_DIMENSION = 1200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_innercamera);
-
+        Log.d("왜안돼","버튼클릭");
         previewView = findViewById(R.id.previewView);
         bCapture = findViewById(R.id.bCapture);
-        bRecord = findViewById(R.id.bRecord);
-        bRecord.setText("start recording"); // Set the initial text of the button
-
+        mImageDetails=findViewById(R.id.ImageDetails);
+        Log.d("왜안돼","버튼클릭");
         bCapture.setOnClickListener(this);
-        bRecord.setOnClickListener(this);
-
+        Log.d("왜안돼","버튼클릭후");
+        Log.d("왜안돼","카메라실행");
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         cameraProviderFuture.addListener(() -> {
             try {
@@ -76,7 +97,7 @@ public class InnerCameraActivity extends AppCompatActivity implements ImageAnaly
                 e.printStackTrace();
             }
         }, getExecutor());
-
+        Log.d("왜안돼","카메라실행후");
     }
 
     Executor getExecutor() {
@@ -85,6 +106,7 @@ public class InnerCameraActivity extends AppCompatActivity implements ImageAnaly
 
     @SuppressLint("RestrictedApi")
     private void startCameraX(ProcessCameraProvider cameraProvider) {
+        Log.d("왜안돼","startCameraX");
         cameraProvider.unbindAll();
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
@@ -95,13 +117,10 @@ public class InnerCameraActivity extends AppCompatActivity implements ImageAnaly
 
         // Image capture use case
         imageCapture = new ImageCapture.Builder()
-                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY) //화질 최적화
                 .build();
 
-        // Video capture use case
-        videoCapture = new VideoCapture.Builder()
-                .setVideoFrameRate(30)
-                .build();
+
 
         // Image analysis use case
         ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
@@ -111,16 +130,131 @@ public class InnerCameraActivity extends AppCompatActivity implements ImageAnaly
         imageAnalysis.setAnalyzer(getExecutor(), this);
 
         //bind to lifecycle:
-        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture, videoCapture);
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture);
     }
 
     @Override
     public void analyze(@NonNull ImageProxy image) {
         // image processing here for the current frame
         Log.d("TAG", "analyze: got the frame at: " + image.getImageInfo().getTimestamp());
+
         image.close();
+
+    }
+    private void callCloudVision(final Bitmap bitmap) throws IOException {
+        // Switch text to loading
+        mImageDetails.setText(R.string.loading_message);
+
+        // Do the real work in an async task, because we need to use the network anyway
+        new AsyncTask<Object, Void, String>() {
+            @SuppressLint("StaticFieldLeak")
+            @Override
+            protected String doInBackground(Object... params) {
+                try {
+                    HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+                    GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+                    Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+                    builder.setVisionRequestInitializer(new
+                            VisionRequestInitializer(CLOUD_VISION_API_KEY));
+                    Vision vision = builder.build();
+
+                    BatchAnnotateImagesRequest batchAnnotateImagesRequest =
+                            new BatchAnnotateImagesRequest();
+                    batchAnnotateImagesRequest.setRequests(new ArrayList<AnnotateImageRequest>() {{
+                        AnnotateImageRequest annotateImageRequest = new AnnotateImageRequest();
+
+                        // Add the image
+                        Image base64EncodedImage = new Image();
+                        // Convert the bitmap to a JPEG
+                        // Just in case it's a format that Android understands but Cloud Vision
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
+                        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+                        // Base64 encode the JPEG
+                        base64EncodedImage.encodeContent(imageBytes);
+                        annotateImageRequest.setImage(base64EncodedImage);
+
+                        // add the features we want
+                        annotateImageRequest.setFeatures(new ArrayList<Feature>() {{
+                            Feature labelDetection = new Feature();
+                            labelDetection.setType("TEXT_DETECTION");
+                            labelDetection.setMaxResults(10);
+                            add(labelDetection);
+                        }});
+
+                        // Add the list of one thing to the request
+                        add(annotateImageRequest);
+                    }});
+
+                    Vision.Images.Annotate annotateRequest =
+                            vision.images().annotate(batchAnnotateImagesRequest);
+                    // Due to a bug: requests to Vision API containing large images fail when GZipped.
+                    annotateRequest.setDisableGZipContent(true);
+                    Log.d(TAG, "created Cloud Vision request object, sending request");
+
+                    long sendMS = System.currentTimeMillis();
+                    BatchAnnotateImagesResponse response = annotateRequest.execute();
+                    //uploadDurationSec = (System.currentTimeMillis() - sendMS) / 1000f;
+                    return convertResponseToString(response);
+
+                } catch (GoogleJsonResponseException e) {
+                    Log.d(TAG, "failed to make API request because " + e.getContent());
+                } catch (IOException e) {
+                    Log.d(TAG, "failed to make API request because of other IOException " +
+                            e.getMessage());
+                }
+                return "Cloud Vision API request failed. Check logs for details.";
+            }
+
+            protected void onPostExecute(String result) {
+                mImageDetails.setText(result);
+            }
+        }.execute();
     }
 
+
+
+    private String convertResponseToString(BatchAnnotateImagesResponse response) {
+        //long spentMS = System.currentTimeMillis() - startTimeMS;
+        //String message = "Recognition results:\n";
+        String message ="";
+        StringBuilder builder = new StringBuilder(message);
+        //builder.append(String.format("(Total spent %.2f secs, including %.2f secs for upload)\n\n", spentMS / 1000f, uploadDurationSec));
+
+        List<EntityAnnotation> labels = response.getResponses().get(0).getTextAnnotations();
+        Log.i("JackTest", "total labels:" + labels.size());
+        if (labels != null) {
+            for (int i = 0; i < labels.size(); i++ ) {
+                EntityAnnotation label = labels.get(i);
+//                if (i == 0) {
+//                    builder.append("Locale: ");
+//                    builder.append(label.getLocale());
+//                }
+                builder.append(label.getDescription());
+                builder.append("\n");
+                //TODO: Draw rectangles later
+                break;
+            }
+        } else {
+            builder.append("nothing");
+        }
+
+        return builder.toString();
+    }
+
+    private class ImageData {
+        private int mWidth, mHeight, mStride;
+        byte[] mBytes;
+
+        ImageData(byte[] bytes, int nWidth, int nHeight, int nStride) {
+            mBytes = bytes;
+            mWidth = nWidth;
+            mHeight = nHeight;
+            mStride = nStride;
+        }
+    }
     @SuppressLint("RestrictedApi")
     @Override
     public void onClick(View view) {
@@ -128,65 +262,10 @@ public class InnerCameraActivity extends AppCompatActivity implements ImageAnaly
             case R.id.bCapture:
                 capturePhoto();
                 break;
-            case R.id.bRecord:
-                if (bRecord.getText() == "start recording"){
-                    bRecord.setText("stop recording");
-                    recordVideo();
-                } else {
-                    bRecord.setText("start recording");
-                    videoCapture.stopRecording();
-                }
-                break;
 
         }
     }
 
-    @SuppressLint("RestrictedApi")
-    private void recordVideo() {
-        if (videoCapture != null) {
-
-            long timestamp = System.currentTimeMillis();
-
-            ContentValues contentValues = new ContentValues();
-            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timestamp);
-            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4");
-
-            try {
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                videoCapture.startRecording(
-                        new VideoCapture.OutputFileOptions.Builder(
-                                getContentResolver(),
-                                MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
-                                contentValues
-                        ).build(),
-                        getExecutor(),
-                        new VideoCapture.OnVideoSavedCallback() {
-                            @Override
-                            public void onVideoSaved(@NonNull VideoCapture.OutputFileResults outputFileResults) {
-                                Toast.makeText(InnerCameraActivity.this, "Video has been saved successfully.", Toast.LENGTH_SHORT).show();
-                            }
-
-                            @Override
-                            public void onError(int videoCaptureError, @NonNull String message, @Nullable Throwable cause) {
-                                Toast.makeText(InnerCameraActivity.this, "Error saving video: " + message, Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                );
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
 
     private void capturePhoto() {
         long timestamp = System.currentTimeMillis();
@@ -195,8 +274,8 @@ public class InnerCameraActivity extends AppCompatActivity implements ImageAnaly
         contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timestamp);
         contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
 
-
-
+        //takePicture(OutputFileOptions, Executor, OnImageSavedCallback)
+        //이 메서드는 캡처된 이미지를 제공된 파일 위치에 저장합니다.
         imageCapture.takePicture(
                 new ImageCapture.OutputFileOptions.Builder(
                         getContentResolver(),
@@ -207,6 +286,7 @@ public class InnerCameraActivity extends AppCompatActivity implements ImageAnaly
                 new ImageCapture.OnImageSavedCallback() {
                     @Override
                     public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        uploadImage(outputFileResults.getSavedUri());
                         Toast.makeText(InnerCameraActivity.this, "Photo has been saved successfully.", Toast.LENGTH_SHORT).show();
                     }
 
@@ -218,4 +298,46 @@ public class InnerCameraActivity extends AppCompatActivity implements ImageAnaly
         );
 
     }
+    //이미지 업로드 기능
+    public void uploadImage(Uri uri) {
+        if (uri != null) {
+            try {
+                // scale the image to save on bandwidth =이미지 크기 조정
+                Bitmap bitmap =
+                        scaleBitmapDown(
+                                MediaStore.Images.Media.getBitmap(getContentResolver(), uri),
+                                MAX_DIMENSION);
+
+                callCloudVision(bitmap);
+                //mMainImage.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                Log.d(TAG, "Image picking failed because " + e.getMessage());
+                Toast.makeText(this, R.string.image_picker_error, Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Log.d(TAG, "Image picker gave us a null image.");
+            Toast.makeText(this, R.string.image_picker_error, Toast.LENGTH_LONG).show();
+        }
+    }
+    private Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
+
+        int originalWidth = bitmap.getWidth();
+        int originalHeight = bitmap.getHeight();
+        int resizedWidth = maxDimension;
+        int resizedHeight = maxDimension;
+
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = (int) (resizedHeight * (float) originalWidth / (float) originalHeight);
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension;
+            resizedHeight = (int) (resizedWidth * (float) originalHeight / (float) originalWidth);
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = maxDimension;
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
+    }
+
 }
