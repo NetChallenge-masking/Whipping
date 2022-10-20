c6acdb34b044a471444e00ed8db5588a5eeb95b1
Choonsikii <20200976@sungshin.ac.kr>
2022-10-20 17:54:14 +0900

    음성안내 1차

---
+++ app/src/main/java/kr/co/whipping/tts_beta.java
@@ -0,0 +1,76 @@
+package com.example.tts_3;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.nl.languageid.LanguageIdentification;
+import com.google.mlkit.nl.languageid.LanguageIdentifier;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText el = findViewById(R.id.editTextTextMultiLine);
+        String text = el.getText().toString();
+        Button bi = findViewById(R.id.button);
+
+        LanguageIdentifier languageIdentifier =
+                LanguageIdentification.getClient();
+        languageIdentifier.identifyLanguage(text)
+                .addOnSuccessListener(
+                        new OnSuccessListener<String>() {
+                            @Override
+                            public void onSuccess(@Nullable String languageCode) {
+                                if (languageCode.equals("und")) {
+                                    int i = Log.i(TAG, "Can't identify language.");
+                                } else {
+                                    Log.i(TAG, "Language: " + languageCode);
+                                }
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Model couldn’t be loaded or other internal error.
+                                // ...
+                            }
+                        });
+
+        bi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int i) {
+                        if(i==TextToSpeech.SUCCESS){
+                            tts.setLanguage(Locale.KOREA);
+                            tts.setSpeechRate(1.0f);
+                            tts.speak(el.getText().toString(), TextToSpeech.QUEUE_ADD, null);
+                        }
+
+                    }
+                });
+            }
+        });
+    }
+}
