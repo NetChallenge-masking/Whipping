7eeeee4d42320a322973f88a5c01a9349b1f2acb
kche1106 <kche011106@gmail.com>
2022-11-05 18:31:16 +0900

    [FEAT] 바코드 이미지 여러장 불러오기

---
+++ app/src/main/java/kr/co/whipping/BarcodeImgAdapter.java
@@ -0,0 +1,49 @@
+package kr.co.whipping;
+
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class BarcodeImgAdapter extends RecyclerView.Adapter<BarcodeImgAdapter.ViewHolder>{
+    private List<Bitmap> img;
+
+    public BarcodeImgAdapter(List<Bitmap> img) {
+        this.img = img;
+    }
+
+    @NonNull
+    @Override
+    public BarcodeImgAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.barcodeimg_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BarcodeImgAdapter.ViewHolder holder, int position) {
+
+        holder.img.setImageBitmap(img.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return img.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView img;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.iv_barcode_background);
+        }
+    }
+
+}
+
