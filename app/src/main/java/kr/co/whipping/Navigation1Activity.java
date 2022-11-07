bde23a0c7b05945895df107b6b3c34811ad5dc35
Choonsikii <20200976@sungshin.ac.kr>
2022-11-08 04:20:59 +0900

    [FEAT] 위치 안내 애니메이션 추가

--- app/src/main/java/kr/co/whipping/Navigation1Activity.java
+++ app/src/main/java/kr/co/whipping/Navigation1Activity.java
@@ -3,6 +3,9 @@ package kr.co.whipping;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Navigation1Activity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class Navigation1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation1);
+
+
+        ImageView ivEvent = findViewById(R.id.wanttogo);
+        Animation anim = AnimationUtils.loadAnimation
+                (getApplicationContext(), R.anim.updown);
+        ivEvent.startAnimation(anim);
     }
 }
